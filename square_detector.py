91e7cd6f848f06db078897edc22b789bf5af26a9
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-01-17 16:07:49 +0100

    trying to debug SquareFinder

--- square_detector.py
+++ square_detector.py
@@ -2,37 +2,95 @@ import itertools
 import operator
 from formulas import *
 
+
 class SquareDetector:
 
     def __init__(self, intersection_pts):
-        # sort the intersections points
+
+        
+        # sort the intersection points
         intersection_pts = sorted(intersection_pts)
 
         # group the points of intersection by the row they're on
         rows = []
         for key,group in itertools.groupby(intersection_pts,operator.itemgetter(0)):
             rows.append(list(group))
-    
+        
+
+        # number of columns MUST be equal to number of rows!
+        assert len(rows)==len(rows[0])
+        N = len(rows)
+        
+        
         # --------- find the squares/bounding boxes identified by the points------------
-        squares = []
-        for row_ctr in range(len(rows)-1):
-            row1 = rows[row_ctr]
-            row2 = rows[row_ctr+1]
+        self.squares = []
+
+        for r in range(len(rows)-1):
+            row1 = rows[r]
+            row2 = rows[r+1]
+
+            print(r, rows[r])
+            print(r+1, rows[r+1])
+            print("-"*100)
             
-            for col in range( min(len(row1), len(row2)) -1 ):
-                print("square start:")
-                print(row1[col] , row1[col+1]  )
-                print(row2[col] , row2[col+1]  )   
+            for col in range( N-1 ):
                 square = (row1[col] , row1[col+1], row2[col] , row2[col+1] )
-                squares.append(square)
+                self.squares.append(square)
         
+
         # filter out squares that are too small
-        areas = [area_square(square) for square in squares]
+        areas = [area_square(square) for square in self.squares]
         mean_area = sum(areas)/len(areas)
-        squares = [square for square in squares if area_square(square) >= mean_area]
+        self.squares = [square for square in self.squares if area_square(square) >= mean_area]
         
-        self.squares = squares
-        print("HELOOOOOOOOOOOOOOOO", len(self.squares))
+
+
+
+def __main__():
+
+    import os
+    import sys
+    import cv2
+    from edge_detector import detect_edges
+    from line_detector import LineDetector
+
+
+    """
+    Unit test.
+    """
+
+    try:
+        path = sys.argv[1]
+    except:
+        path = os.path.join(".", "images", "sudoku3.png")
+    
+    # load the image
+    img = cv2.imread(path)
+
+    #Find edge-points
+    edges = detect_edges(img)
+
+    #Find lines from points
+    my_line_detector = LineDetector(edges)
+
+    #Find the points of intersection of the grid lines
+    intersection_pts = my_line_detector.intersections()
+
+    # Find the squares/bounding-boxes
+    squares = SquareDetector(intersection_pts).squares
+
+    print(f"Found {len(squares)} squares.")
+
+
+    
+
+
+
+
+
+if __name__ == "__main__":
+    __main__()
+
 
 
 
