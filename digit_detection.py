fdd206f803dd24f7acf6514214ebb25eb48b6468
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-01-19 10:04:04 +0100

    limited char-whitelist to digits

--- digit_detection.py
+++ digit_detection.py
@@ -1,15 +1,21 @@
 import cv2 
 import pytesseract
+
 #pytesseract.pytesseract.tesseract_cmd = r'C:\Users\user\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
 
 def image_to_digit(image):
-    custom_config = r'--oem 3 --psm 6' # Adding custom options
+    #custom_config = r'--oem 3 --psm 6' # Adding custom options
+
+    custom_config = r'--oem 3 --psm 6 -c tessedit_char_whitelist=0123456789' # Adding custom options
+
     s = pytesseract.image_to_string(image, config=custom_config)
     try:
         return int(s)
     except:
         pass    
+
+
 
 
 
